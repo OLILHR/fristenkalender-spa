85be29d16191232d776c75f3d73dfba672fb0ba5
olli <info@oliverlahr.de>
2024-12-18 23:15:26 +0100

    Update import rules of eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,81 @@
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import sveltePlugin from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import prettierConfig from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import importPlugin from "eslint-plugin-import";
+
 export default [
+  js.configs.recommended,
   {
-    ignores: [
-      "package.json",
-      "package-lock.json",
-      ".svelte-kit/*",
-      "node_modules/*",
-      "test-results/*",
-      "build/*",
-    ],
+    files: ["**/*.{js,ts,svelte}"],
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      svelte: sveltePlugin,
+      "simple-import-sort": simpleImportSort,
+      import: importPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        extraFileExtensions: [".svelte"],
+      },
+      globals: {
+        // Browser globals
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        fetch: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        Blob: "readonly",
+        URL: "readonly",
+        URLSearchParams: "readonly",
+        HTMLDivElement: "readonly",
+        CustomEvent: "readonly",
+        // DOM types
+        Event: "readonly",
+        EventTarget: "readonly",
+        HTMLElement: "readonly",
+        HTMLInputElement: "readonly",
+        HTMLSelectElement: "readonly",
+        MouseEvent: "readonly",
+        // Node.js globals
+        process: "readonly",
+      },
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...sveltePlugin.configs.recommended.rules,
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "svelte/no-at-html-tags": "off",
+      "no-undef": "error",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
   },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
+  prettierConfig,
 ];
