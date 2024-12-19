4a23279a37549332929881897461bf5d46b43425
olli <info@oliverlahr.de>
2024-12-19 12:20:06 +0100

    Move index.ts into `lib/components`

---
+++ src/lib/components/index.ts
@@ -0,0 +1,4 @@
+// shared components
+export { default as Footer } from "$lib/components/shared/footer.svelte";
+export { default as IconHeart } from "$lib/components/shared/icon-heart.svelte";
+export { default as IconLogo } from "$lib/components/shared/icon-logo.svelte";
