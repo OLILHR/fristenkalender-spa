c11e1fd2c9d02bfbe9d37b46f6d94dd5895d3051
olli <144932831+OLILHR@users.noreply.github.com>
2024-12-19 15:44:53 +0100

    feat: add `fristenkalender` route (#11)
    * Add `fristenkalender` route
    
    * Add auth button to `fristenkalender` route

---
+++ src/routes/fristenkalender/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "$src/app.scss";
+
+  import { AuthButton } from "$lib/components";
+</script>
+
+<div class="relative">
+  <div class="absolute top-4 right-4 z-10">
+    <AuthButton background="bg-secondary" textColor="text-tint" />
+  </div>
+</div>
+
+<main>
+  <slot />
+</main>
+
+<style></style>
