621dd4ffbf94592aee9be27d63f84ccec2b712a2
olli <info@oliverlahr.de>
2024-12-19 15:41:35 +0100

    Add auth button to `fristenkalender` route

--- src/lib/components/shared/auth-button.svelte
+++ src/lib/components/shared/auth-button.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import { IconLogin, IconLogout } from "$lib/components";
+
+  export let background = "bg-tint";
+  export let textColor = "text-secondary";
 </script>
 
 <div class="flex items-center gap-4">
   <button
-    class="flex items-center gap-2 rounded-full bg-tint text-[16px] py-3 px-5 text-secondary"
+    class="flex items-center gap-2 rounded-full {background} {textColor} text-[16px] py-3 px-5"
   >
     Abmelden <IconLogout />
   </button>
   <button
-    class="flex items-center gap-2 rounded-full bg-tint text-[16px] py-3 px-5 text-secondary"
+    class="flex items-center gap-2 rounded-full {background} {textColor} text-[16px] py-3 px-5"
   >
     Anmelden <IconLogin />
   </button>
--- src/routes/fristenkalender/+layout.svelte
+++ src/routes/fristenkalender/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
   import "$src/app.scss";
+
+  import { AuthButton } from "$lib/components";
 </script>
+
+<div class="relative">
+  <div class="absolute top-4 right-4 z-10">
+    <AuthButton background="bg-secondary" textColor="text-tint" />
+  </div>
+</div>
 
 <main>
   <slot />
