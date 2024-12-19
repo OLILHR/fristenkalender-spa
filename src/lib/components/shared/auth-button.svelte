41c4d3c72cfc7a704779af055506fcba9ed9ab2c
olli <info@oliverlahr.de>
2024-12-19 16:06:45 +0100

    Add dummy auth user for local development

--- src/lib/components/shared/auth-button.svelte
+++ src/lib/components/shared/auth-button.svelte
@@ -1,19 +1,59 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   import { IconLogin, IconLogout } from "$lib/components";
+  import auth from "$src/auth/authService";
+  import { isAuthenticated, user } from "$src/store";
 
   export let background = "bg-tint";
   export let textColor = "text-secondary";
+
+  let userEmail: string = "";
+
+  onMount(async () => {
+    if (
+      window.location.search.includes("code=") ||
+      window.location.search.includes("state=")
+    ) {
+      await auth.handleRedirectCallback();
+      window.history.replaceState({}, document.title, window.location.pathname);
+    }
+
+    await auth.checkAuth();
+  });
+
+  $: if ($isAuthenticated && $user) {
+    userEmail = $user.email || "";
+  } else {
+    userEmail = "";
+  }
+
+  async function login() {
+    await auth.loginWithRedirect();
+  }
+
+  async function logout() {
+    await auth.logout();
+  }
 </script>
 
 <div class="flex items-center gap-4">
-  <button
-    class="flex items-center gap-2 rounded-full {background} {textColor} text-[16px] py-3 px-5"
-  >
-    Abmelden <IconLogout />
-  </button>
-  <button
-    class="flex items-center gap-2 rounded-full {background} {textColor} text-[16px] py-3 px-5"
-  >
-    Anmelden <IconLogin />
-  </button>
+  {#if $isAuthenticated && userEmail}
+    <span class="text-[16px] text-white">{userEmail}</span>
+  {/if}
+  {#if $isAuthenticated}
+    <button
+      on:click={logout}
+      class="flex items-center gap-2 rounded-full {background} {textColor} text-[16px] py-3 px-5"
+    >
+      Abmelden <IconLogout />
+    </button>
+  {:else}
+    <button
+      on:click={login}
+      class="flex items-center gap-2 rounded-full {background} {textColor} text-[16px] py-3 px-5"
+    >
+      Anmelden <IconLogin />
+    </button>
+  {/if}
 </div>
