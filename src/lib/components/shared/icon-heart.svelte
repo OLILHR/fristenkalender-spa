527229d08a524dca81e9926e9f18f26ee2423c6d
olli <info@oliverlahr.de>
2024-12-21 00:31:59 +0100

    Adjust icon stroke colors

--- src/lib/components/shared/icon-heart.svelte
+++ src/lib/components/shared/icon-heart.svelte
@@ -4,7 +4,7 @@
   height="20"
   viewBox="0 0 24 24"
   fill="none"
-  stroke="currentColor"
+  stroke="rgb(0 0 0 / 0.7)"
   stroke-width="2"
   stroke-linecap="round"
   stroke-linejoin="round"
--- src/lib/components/shared/icon-login.svelte
+++ src/lib/components/shared/icon-login.svelte
@@ -4,7 +4,7 @@
   height="22"
   viewBox="0 0 24 24"
   fill="none"
-  stroke="currentColor"
+  stroke="rgb(0 0 0 / 0.7)"
   stroke-width="2"
   stroke-linecap="round"
   stroke-linejoin="round"
